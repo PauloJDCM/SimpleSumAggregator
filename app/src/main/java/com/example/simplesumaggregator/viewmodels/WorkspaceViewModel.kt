37f02929ed35a11b52347579c9aa2e280fb4b084
Paulo Miranda <paulojdcmiranda@gmail.com>
2025-07-10 16:55:51 +0200

    Added quantity positive validation

--- app/src/main/java/com/example/simplesumaggregator/viewmodels/WorkspaceViewModel.kt
+++ app/src/main/java/com/example/simplesumaggregator/viewmodels/WorkspaceViewModel.kt
@@ -7,6 +7,7 @@ import com.example.simplesumaggregator.MessageBuilder
 
 class WorkspaceViewModel(entries: List<Entry> = listOf()) : ViewModel() {
     private val _entries = entries.toMutableStateList()
+    private val _maxIdLength = 10
 
     val entries get() = _entries
 
@@ -18,14 +19,16 @@ class WorkspaceViewModel(entries: List<Entry> = listOf()) : ViewModel() {
      * @param quantity The quantity of the item.
      * @return An error message if validation fails, otherwise null.
      */
-    fun addEntry(groupId: String, itemId: String, quantity: String) : String? {
+    fun addEntry(groupId: String, itemId: String, quantity: String): String? {
         val entryGroupId = groupId.trim().ifBlank { null }
         val entryItemId = itemId.trim()
         val entryQuantity = quantity.trim().toIntOrNull()
 
         val errorBuilder = MessageBuilder()
+        if (entryGroupId != null && entryGroupId.length > _maxIdLength) errorBuilder.addMessage("Group ID cannot be longer than $_maxIdLength characters!")
         if (entryItemId.isBlank()) errorBuilder.addMessage("Item ID cannot be blank!")
-        if (entryQuantity == null) errorBuilder.addMessage("Quantity must be a number!")
+        if (entryItemId.length > _maxIdLength) errorBuilder.addMessage("Item ID cannot be longer than $_maxIdLength characters!")
+        if (entryQuantity == null || entryQuantity <= 0) errorBuilder.addMessage("Quantity must be a positive number!")
         if (errorBuilder.hasMessages) return errorBuilder.build()
 
         _entries.add(Entry(entryGroupId, itemId, quantity.toInt()))
